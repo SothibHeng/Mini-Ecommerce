dc6bc960cbed1138bdf9f5eb290c32af469e93dd
ShothibHeng <sothibheng23@gmail.com>
2024-04-06 19:01:20 +0700

    fix error

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -11,19 +11,19 @@ export default function Page() {
      Welcome to our E-Commerce platform, where shopping meets convenience in the digital age. Our website serves as your virtual marketplace, bringing an extensive array of products and services right to your fingertips.
      </p>
      <p className='mt-6 text-gray-700'>
-     Explore our meticulously curated product catalog, thoughtfully categorized to simplify your browsing experience. From fashion to electronics, home essentials to gifts, we've got something for everyone.
+     Explore our meticulously curated product catalog, thoughtfully categorized to simplify your browsing experience. From fashion to electronics, home essentials to gifts, we&apos;ve got something for everyone.
      </p>
      <p className='mt-6 text-gray-700'>
-     With our user-friendly interface, seamlessly navigate through our virtual aisles, adding your favorite items to your personalized shopping cart. When you're ready to complete your purchase, our secure checkout process ensures peace of mind with various payment options available to suit your preferences.
+     With our user-friendly interface, seamlessly navigate through our virtual aisles, adding your favorite items to your personalized shopping cart. When you&apos;re ready to complete your purchase, our secure checkout process ensures peace of mind with various payment options available to suit your preferences.
      </p>
      <p className='mt-6 text-gray-700'>
-     Whether you're a seasoned shopper or just starting your online retail journey, our platform caters to your needs. Register for an account to unlock additional features like order tracking and personalized recommendations, or simply proceed as a guest for a quick and hassle-free experience.
+     Whether you&apos;re a seasoned shopper or just starting your online retail journey, our platform caters to your needs. Register for an account to unlock additional features like order tracking and personalized recommendations, or simply proceed as a guest for a quick and hassle-free experience.
      </p>
      <p className='mt-6 text-gray-700'>
      At the heart of our platform lies a commitment to quality and customer satisfaction. Each product is meticulously selected, and user reviews and ratings provide valuable insights to guide your purchasing decisions.
      </p>
      <p className='mt-6 text-gray-700'>
-     Our responsive design ensures a seamless shopping experience across devices, whether you're browsing from your desktop, tablet, or smartphone.
+     Our responsive design ensures a seamless shopping experience across devices, whether you&apos;re browsing from your desktop, tablet, or smartphone.
      </p>
      <p className='mt-6 text-gray-700'>
      Behind the scenes, our dedicated team works tirelessly to ensure smooth order management, from processing payments to handling shipping and returns. Your satisfaction is our priority, and we strive to exceed your expectations at every step of your shopping journey.
